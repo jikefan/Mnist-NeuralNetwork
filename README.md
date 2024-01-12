036e4bfe326ac6ab6f52d3073f69d6065be42200
jikefan <1791504346@qq.com>
2024-01-12 18:33:25 +0800

    init project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # NumpyMnist-NeuralNetwork
 用Numpy实现神经网络进行Mnist手写体的识别Demo
+
+## 环境
+
+- python 3.11.7
+- numpy 1.26.3
--- app/main.py
+++ app/main.py
@@ -1,5 +1,34 @@
+import os
+import sys
+import numpy as np
+
+
+def get_data_path():
+    # 获取脚本的主模块文件路径
+    main_script_path = sys.modules['__main__'].__file__
+    # 获取该文件所在的目录
+    project_root = os.path.dirname(os.path.abspath(main_script_path)) # type: ignore 忽略Pylance的报红
+    # 数据所在路径
+    data_path = os.path.join(project_root, "../data")
+    
+    return data_path
+
 def main():
     print('Hello Neural Network!')
+    
+    test_set_filename = 'testset.npz'
+    train_set_filename = 'trainset.npz'
+    
+    data_path = get_data_path()
+    
+    test_set_path = os.path.join(data_path, test_set_filename)
+    train_set_path = os.path.join(data_path, train_set_filename)
+    
+    test_set = np.load(test_set_path)
+    train_set = np.load(train_set_path)
+    
+    print(test_set)
+
 
 
 if __name__ == "__main__":
